107e662028b128fe825637b59a568997e4653e2c
rc-gss <romyla.gss@gmail.com>
2019-06-18 18:08:29 +0800

    minor updates

--- js/script.js
+++ js/script.js
@@ -26,6 +26,35 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
 }
 
+// tabs gallery
+// var index = 1;
+// showSlides(index);
+
+// function plusSlides(n) {
+//   showSlides(index += n);
+// }
+
+// function currentSlide(n){
+//   showSlides(index = n);
+// }
+
+// function showSlides(n){
+//   var i;
+//   var slides = document.getElementsByClassName("img--full__slider");
+//   var thumbs = document.getElementsByClassName("tab__img--thumbnail");
+//   if(n > showSlides.length){
+//     index = 1;
+//   }
+//   if (n < 1){
+//     index =
+//   }
+
+//   for(i = 0; i < thumbs.length; i++){
+//     thumbs[i].className = thumbs[i].className.replace("  active", "");
+//   }
+//   thumbs[slideiNdex-1].className += " active";
+// }
+
 function timer() {
   var countDown = new Date("December 31 2019 23:59:59").getTime();
 
@@ -60,5 +89,5 @@ function addLoadEvent(func) {
     };
   }
 }
-// addLoadEvent(slider);
+
 addLoadEvent(timer);
